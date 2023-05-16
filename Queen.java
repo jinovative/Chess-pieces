4a811d474951178499e7ac2bde98ea08d4750a3d
Jin <jinyung7428@gmail.com>
2023-05-16 00:14:20 -0400

    made interface guidelines

--- Queen.java
+++ Queen.java
@@ -1,6 +1,34 @@
 interface ChessPiece {
-
+  int getRow();
+  int getColumn();
+  Color getColor();
+  boolean canMove(int row, int col);
+  boolean canKill(ChessPiece piece);
 }
 
-public class Queen {
+public class Queen implements ChessPiece {
+  @Override
+  public int getRow() {
+    return 0;
+  }
+
+  @Override
+  public int getColumn() {
+    return 0;
+  }
+
+  @Override
+  public Color getColor() {
+    return null;
+  }
+
+  @Override
+  public boolean canMove(int row, int col) {
+    return false;
+  }
+
+  @Override
+  public boolean canKill(ChessPiece piece) {
+    return false;
+  }
 }
